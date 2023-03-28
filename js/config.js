f3d26726a0fd8e02ca3dc132eef66edb90739398
Diego Bisego <diegobisego@gmail.com>
2023-03-28 00:35:56 -0300

    se agrega url para peticiones  glitch

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const instance = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://glitch.com/~distribuidorabisego/api',
   timeout: 5000, // tiempo máximo de espera de respuesta (en milisegundos)
   headers: {
     'Content-Type': 'application/json'
